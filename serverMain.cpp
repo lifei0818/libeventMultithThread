b7be8bc42ba5f5a903dd52cd4a67e343d1a2643f
lifei <758417483@qq.com>
2020-08-15 15:48:45 +0800

    server get port from config

--- serverMain.cpp
+++ serverMain.cpp
@@ -4,26 +4,7 @@
 
 int main()
 {
-#ifdef WIN32
-	evthread_use_windows_threads();
-	WinSockInit();
-#else
-	evthread_use_pthreads();
-#endif // WIN32
-
-	event_enable_debug_mode();
-	//替换记录日志的默认函数
-	event_enable_debug_logging(EVENT_DBG_ALL);
-	event_set_log_callback(log_callback);
-	//server();
 	LibEvtServer libEvt;
-	bool bRt = libEvt.init();
-	if (bRt)
-	{
-		int nPort = 12345;
-		libEvt.listen(&nPort);
-	}
-	StudentManager man;
 	while (1)
     {
         BaseLib::OS::sleep(SEC_TIME(1));
